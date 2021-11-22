72993076d53c143af68d27300ffcdc359e3ed452
salexperrucci <salex.perrucci@itis.pr.it>
2021-11-22 21:21:49 +0100

    Almost Finished Project

--- index.js
+++ index.js
@@ -3,13 +3,63 @@ const del = document.getElementById("DEL");
 const input = document.getElementById("text");
 const numbers = document.getElementsByClassName("numero");
 const rest = document.getElementById("%");
+const moltiplication = document.getElementById("x");
+const division = document.getElementById("/");
+const add = document.getElementById("+");
+const minus = document.getElementById("-");
+const textBefore = document.getElementById("before");
+const equal = document.getElementById("=");
 
 for(let i = 0; i < numbers.length; i++) numbers[i].addEventListener("click", () => input.value === "0" ? input.value = numbers[i].innerHTML : input.value += numbers[i].innerHTML);
 
+let vecchia = 0;
+let operator = "";
+
+add.addEventListener("click", function () {
+    vecchia = parseFloat(input.value);
+    input.value = "0";
+    operator = "+";
+    before.innerHTML = vecchia + "+";
+});
+
+minus.addEventListener("click", function () {
+    vecchia = parseFloat(input.value);
+    input.value = "0";
+    operator = "-";
+    before.innerHTML = vecchia + "-";
+});
+
+division.addEventListener("click", function () {
+    vecchia = parseFloat(input.value);
+    input.value = "0";
+    operator = "/";
+    before.innerHTML = vecchia + "/";
+});
+
+moltiplication.addEventListener("click", function () {
+    vecchia = parseFloat(input.value);
+    input.value = "0";
+    operator = "*";
+    before.innerHTML = vecchia + "*";
+});
+
 input.addEventListener("keypress", (e) => e.which <= 32 || e.which > 57 ? e.preventDefault() : null);
-reset.addEventListener("click", () => input.value = "0");
+reset.addEventListener("click", function () { input.value = "0"; before.innerHTML = ""});
 del.addEventListener("click", function () {
     input.value = input.value.substr(0, input.value.length - 1);
     if(input.value.length == 0) input.value = "0";
+});
+rest.addEventListener("click", function() {
+    vecchia = parseFloat(input.value);
+    input.value = "0";
+    operator = "%";
+});
+
+equal.addEventListener("click", function () {
+    if(operator === "%") before.innerHTML += input.value, input.value = vecchia % parseFloat(input.value);
+    else if(operator === "/") before.innerHTML += input.value, input.value = vecchia / parseFloat(input.value);
+    else if(operator === "*") before.innerHTML += input.value, input.value = vecchia * parseFloat(input.value);
+    else if(operator === "+") before.innerHTML += input.value, input.value = vecchia + parseFloat(input.value);
+    else if(operator === "-") before.innerHTML += input.value, input.value = vecchia - parseFloat(input.value);
 });
 
